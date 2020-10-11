296b46b2da92cc28551de3193f70ab1982351a64
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2020-10-11 16:49:17 -0500

    Update BoostFibonacci.c

--- Object/BoostFibonacci.c
+++ Object/BoostFibonacci.c
@@ -100,7 +100,6 @@ double CanonicalRankDecomposition (  sinc_label  f0 ,double * coeff,   division 
     blockA(&c1.rt, blockCopyBlock);
     blockA(&c1.rt, blockTransferBasisblock);
     blockA(&c1.rt, blockMatrixElementsblock);
-    blockA(&c1.rt, blockParallelMultiplyblock);
     blockA(&c1.rt, blockPrintStuffblock);
     blockA(&c1.rt, blockTotalVectorParallelBlock);
 
