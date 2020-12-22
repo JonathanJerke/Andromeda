ad826e246a7168731e92ba90c667882209b12582
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2020-12-21 19:16:43 -0600

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#run in Andromeda directory for best effect
+
+##compile first
+cd Object
+make olympics
+cp olympics andromeda
+
+make libandromeda.so
+export LDFLAGS=`pwd`
+export LD_LIBRARY_PATH=`pwd`
+cd ..
+
+cd andromedaPy
+python3 setup.py build_ext -i
+cd ..
+
+##run python3 
+##import andromedaPy as apy
+##code still under development
