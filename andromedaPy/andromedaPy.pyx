2ca3ae496126ec279a3d45502ca52a58474bbbd6
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2020-12-23 05:56:24 -0600

    Add files via upload

--- andromedaPy/andromedaPy.pyx
+++ andromedaPy/andromedaPy.pyx
@@ -22,6 +22,7 @@
 #*   *   You should have received a copy of the GNU General Public License
 #*   *   along with Andromeda.  If not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
 cimport andromedaPy
 include "system.pxi"
 
