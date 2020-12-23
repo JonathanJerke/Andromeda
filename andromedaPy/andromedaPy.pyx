4982facbd11560f157e7ac81d735022c585166e9
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2020-12-23 05:23:19 -0600

    Add files via upload
    beginning work on fundamentals

--- andromedaPy/andromedaPy.pyx
+++ andromedaPy/andromedaPy.pyx
@@ -26,21 +26,21 @@ cimport andromedaPy
 from constants cimport floata
 from constants cimport field
 from constants cimport calculation
+
 from Model cimport initCal
 from Model cimport initField
 from Model cimport iModel
 from Model cimport fModel
+
 from constants cimport dimensions_label
+from constants cimport metric_label
+
 from constants cimport basisElementType
 from constants cimport componentType
+from constants cimport functionType
+from constants cimport metricType
 
-cdef enum componentType:
-    nullComponent,
-    spatialComponent1,
-    spatialComponent2,
-    spatialComponent3,
-
-cdef class base:
+cdef class galaxy:
     cdef calculation calculation
     cdef field field
 
@@ -56,35 +56,170 @@ cdef class base:
         """
         return dimensions_label(lattice = lattice , attack = attack, origin = origin, anchor = anchor )
 
-    def comps(self, i=1,periodic = False):
-        """Returns new component enum a linear dimension.
+    def comps(self, N , inc=1, i=1,periodic = False):
+        """Returns new component enum for a linear dimension.
         """
         if i <= 0 :
-            return componentType.nullComponent
+            return [N,inc,componentType.nullComponent]
         if not periodic:
             if i == 1 :
-                return componentType.spatialComponent1
+                return [N,inc,componentType.spatialComponent1]
             elif i == 2 :
-                return componentType.spatialComponent2
+                return [N,inc,componentType.spatialComponent2]
             elif i == 3 :
-                return componentType.spatialComponent3
+                return [N,inc,componentType.spatialComponent3]
                 
         if periodic:
             if i == 1 :
-                return componentType.periodicComponent1
+                return [N,inc,componentType.periodicComponent1]
             elif i == 2 :
-                return componentType.periodicComponent2
+                return [N,inc,componentType.periodicComponent2]
             elif i == 3 :
-                return componentType.periodicComponent3
+                return [N,inc,componentType.periodicComponent3]
 
-        else :
-            return componentType.nullComponent
+    	return [0,inc,componentType.nullComponent]
 
-    def comps(self,component, dims, count1Inc=1, basis=basisElementType.SincBasisElement):
-        """Returns new info for parameters for a component bundle.
-        """
-        nbody = len(dims)
-        if (nbody > 3) :
-            print('warning: nbody > 3')
-            return self
-    
+
+	def bases(self, i = 1 ):
+		"""Returns enumeration of basis types
+		"""
+		if i <= 0 :
+            return basisElementType.nullBasisElement
+        if not periodic:
+            if i == 1 :
+                return basisElementType.SincBasisElement
+            elif i == 2 :
+                return basisElementType.GaussianBasisElement
+            elif i == 3 :
+                return basisElementType.DiracDeltaElement
+            elif i == 4 :
+                return basisElementType.StateBasisElement
+            elif i == 5 :
+                return basisElementType.overlapBasisElement
+            
+    	return basisElementType.nullBasisElement
+
+
+    def spaces(self,labels, comps, dims, bases ):
+        """Absolute definition of space
+        Three equal length vectors are conjoined.
+        
+        Parameters
+        ----------
+        labels : [int]
+        		Per Space , bundle names
+        comps  : [self.comps]
+        		Per Space , component lengths
+        dims   : [[self.dims]]
+        		Per Space and Particle , float character
+        bases  : [self.bases]
+        		Per Space , type
+        Sets internal clock of definitions
+        
+        Returns 
+        -------
+        self
+    	"""
+		if len(comps) == len(dims) :
+			if len(comps) == len(bases):
+				if len(comps) <= SPACE:
+					if self.field.f.bootedMemory == 1 :
+						print("warning, already booted")
+						return self
+
+					"""Define 
+					"""		
+					
+					for (space,comp) in enumerate(comps):
+						count = 0
+						for labelish in labels:
+							if labelish == labels[space]:
+								count += 1
+						self.field.f.canon[space].component = count				
+						self.field.f.canon[space].space = comp[0]
+						self.field.f.canon[space].count1Basis = comp[1]
+						self.field.f.canon[space].count1Inc = comp[2]
+					for (space,label) in enumerate(labels):
+						self.field.f.canon[space].label = label
+					for (space,dim) in enumerate(dims):
+						for (body, particle) in enumerate(dim):
+							self.field.f.canon[space].particle[body+1] = particle
+						self.field.f.canon[space].body = len(dim)
+					for (space,base) in enumerate(bases):
+						self.field.f.canon[space].basis = base
+						
+		return self	
+					
+	def i(self):
+		"""Initiate allocation, do not overwrite
+		"""
+		if self.field.f.bootedMemory == 1 :
+			print("warning, already booted")
+			return self
+		iModel(&self.calculations, &self.field)
+		return self			
+					
+	def metric(self, funcDesc, intervalDesc, betas ):
+		"""Metric definition by description
+		
+		Parameters
+		----------
+		funcDesc : str
+		intervalDesc : str
+		betas : [int]
+		
+		Returns
+		-------
+		metric_label
+		
+		"""
+		funcNames = dict(
+			{'null':functionType.nullFunction,'Pseudo':functionType.Pseudo,
+			'Yukawa':functionType.Yukawa,'Coulomb':functionType.Coulomb,
+			'Morse':functionType.Morse,'LennardJones':functionType.LennardJones,
+			'LDA':functionType.LDA,'BLYP':functionType.BLYP,
+			'Gaussian':functionType.Gaussian}
+		)
+		
+		intervalName = dict({'dirac':metricType.dirac,
+							'separateDirac':metricType.separateDirac,
+							'interval':metricType.separateDirac,
+							'semiDefinite':metricType.semiIndefinite,
+							'pureInterval':metricType.pureInterval,
+							'pureSemiIndefinite':metricType.pureSemiIndefinite}
+		)
+		
+		
+		
+		metric = metric_label()
+		for space in range(SPACE):
+			metric.pow[space] = 0
+			metric.powB[space] = 0
+			metric.deriv[space] = 0
+		
+		if len(betas)==1:
+			if intervalName[intervalDesc] in [metricType.dirac,metricType.separateDirac,
+							metricType.semiIndefinite,metricType.pureSemiIndefinite]:
+							
+				"""Define
+				"""				
+				metric.fn = funcNames[funcDesc]
+				metric.metric = intervalName[intervalDesc]
+				metric.beta[0] = betas
+				metric.beta[1] = -1
+		elif len(betas) == 2:
+			if intervalName[intervalDesc] in [metricType.pureInterval
+													,metricType.semiIndefinite]:
+					
+				"""Define
+				"""			
+				metric.fn = funcNames[funcDesc]
+				metric.metric = intervalName[intervalDesc]
+				metric.beta[0] = betas[0]
+				metric.beta[1] = betas[1]
+		else:
+			print('betas: len')
+		
+		return metric
+					
+					
