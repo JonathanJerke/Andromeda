853c8d61a90c3a965394221bc4cd4d287d7cf170
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-01-03 07:27:00 -0600

    Add files via upload

--- andromedaPy/andromedaPy.pyx
+++ andromedaPy/andromedaPy.pyx
@@ -101,46 +101,93 @@ cdef class galaxy:
 		bootShell(1, [str(filepy).encode('utf-8')],&self.calculation,&self.field)
 		return self
         
-	def dims(self, lattice:floata, attack:floata =0.5, origin:floata =0.0,
+	def dims(self, lattice:floata = 1, attack:floata =0.5, origin:floata =0.0,
 												 anchor:floata =0.5 ):
-		"""Returns new floata parameters for a linear dimension.
+		"""if not booted Returns new floata parameters for a linear dimension.
+		else returns dims in current galaxy.
+		
+		Parameters
+		----------
+		lattice : floata 
+		attack  : floata
+		origin  : floata
+		anchor  : floata		
+		
+		Returns
+		-------
+		either one dimensions_label or all
 		"""
-		return dimensions_label(lattice = lattice , attack = attack, 
+		if not self.isbooted():
+			return dimensions_label(lattice = lattice , attack = attack, 
 									origin = origin, anchor = anchor )
-
-	def comps(self, N , inc=1, i=1,periodic = False):
+		else :
+			all = []
+			for space in range(SPACE):
+				if self.field.f.canon[space].body != bodyType.nada:
+					some = []
+					for body in range(1,self.field.f.canon[space].body+1):
+						particle = self.field.f.canon[space].particle[body]
+						particle.origin += ( 
+		particle.lattice*(self.field.f.canon[space].count1Basis-1)*particle.anchor)	
+						some += [particle]
+					all += [some]
+			return all
+			
+	def comps(self, N = 1 , inc=1, i=1,periodic = False):
 		"""Returns new component enum for a linear dimension.
 		"""
-		if i <= 0 :
-			return [N,inc,componentType.nullComponent]
-		if not periodic:
-			if i == 1 :
-				return [N,inc,componentType.spatialComponent1]
-			elif i == 2 :
-				return [N,inc,componentType.spatialComponent2]
-			elif i == 3 :
-				return [N,inc,componentType.spatialComponent3]
-                
-		if periodic:
-			if i == 1 :
-				return [N,inc,componentType.periodicComponent1]
-			elif i == 2 :
-				return [N,inc,componentType.periodicComponent2]
-			elif i == 3 :
-				return [N,inc,componentType.periodicComponent3]
+		if not self.isbooted():
+			if i <= 0 :
+				return [N,inc,componentType.nullComponent]
+			if not periodic:
+				if i == 1 :
+					return [N,inc,componentType.spatialComponent1]
+				elif i == 2 :
+					return [N,inc,componentType.spatialComponent2]
+				elif i == 3 :
+					return [N,inc,componentType.spatialComponent3]
+			if periodic:
+				if i == 1 :
+					return [N,inc,componentType.periodicComponent1]
+				elif i == 2 :
+					return [N,inc,componentType.periodicComponent2]
+				elif i == 3 :
+					return [N,inc,componentType.periodicComponent3]
+		else:
+			all = []
+			for space in range(SPACE):
+				if self.field.f.canon[space].body != bodyType.nada:
+					all += [[self.field.f.canon[space].count1Basis,self.field.f.canon[space].count1Inc,self.field.f.canon[space].component]]
+			if len ( all ) > 0 :
+				return all
+			else :
+				return [0,inc,componentType.nullComponent]
 
-		return [0,inc,componentType.nullComponent]
-
-
-	def bases(self, desc : str ):
+	def bases(self, desc : str = 'Sinc' ):
 		"""Returns enumeration of basis types
 		"""
-		names = dict({'Sinc':basisElementType.SincBasisElement,
+		if not self.isbooted():
+			names = dict({'Sinc':basisElementType.SincBasisElement,
 					'Gaussian':basisElementType.GaussianBasisElement,
 					'Dirac':basisElementType.DiracDeltaElement,
 					'State':basisElementType.StateBasisElement,
 					'overlap':basisElementType.overlapBasisElement})
-		return names[desc]
+			return names[desc]
+		else :
+			all = []
+			for space in range(SPACE):
+				if self.field.f.canon[space].body != bodyType.nada:
+					all += [self.field.f.canon[space].basis]
+			return all
+
+	def labels(self):
+		"""Returns enumeration of basis types
+		"""
+		all = []
+		for space in range(SPACE):
+			if self.field.f.canon[space].body != bodyType.nada:
+				all += [self.field.f.canon[space].label]
+		return all
 
 	def spaces(self,labels:[inta], comps, dims, bases ):
 		"""Absolute definition of space
@@ -162,44 +209,42 @@ cdef class galaxy:
         -------
         self
 		"""
-		if len(comps) == len(dims) :
-			if len(comps) == len(bases):
-				if len(comps) <= SPACE:
-					if self.field.f.bootedMemory == 1 :
-						print("warning, already booted")
-						return self
-					"""Define 
-					"""		
+		if not self.isbooted():
+			if len(comps) == len(dims) :
+				if len(comps) == len(bases):
+					if len(comps) <= SPACE:
+						if self.field.f.bootedMemory == 1 :
+							print("warning, already booted")
+							return self
+						"""Define 
+						"""		
 					
-					for (space,comp) in enumerate(comps):
-						count = 0
-						for labelish in labels:
-							if labelish == labels[space]:
-								count += 1
-						self.field.f.canon[space].component = count				
-						self.field.f.canon[space].space = comp[2]
-						self.field.f.canon[space].count1Basis = comp[0]
-						self.field.f.canon[space].count1Inc = comp[2]
-					for (space,label) in enumerate(labels):
-						self.field.f.canon[space].label = label
-					for (space,dim) in enumerate(dims):
-						for (body, particle) in enumerate(dim):
-							self.field.f.canon[space].particle[body+1] = particle
-							self.field.f.canon[space].particle[body+1].origin -= ( 
+						for (space,comp) in enumerate(comps):
+							count = 0
+							for labelish in labels:
+								if labelish == labels[space]:
+									count += 1
+							self.field.f.canon[space].component = count				
+							self.field.f.canon[space].space = comp[2]
+							self.field.f.canon[space].count1Basis = comp[0]
+							self.field.f.canon[space].count1Inc = comp[2]
+						for (space,label) in enumerate(labels):
+							self.field.f.canon[space].label = label
+						for (space,dim) in enumerate(dims):
+							for (body, particle) in enumerate(dim):
+								self.field.f.canon[space].particle[body+1] = particle
+								self.field.f.canon[space].particle[body+1].origin -= ( 
 		particle['lattice']*(self.field.f.canon[space].count1Basis-1)*particle['anchor'])	
-						if len(dim)==1:
-							self.field.f.canon[space].body = bodyType.one
-						elif len(dim)==2:
-							self.field.f.canon[space].body = bodyType.two
-						elif len(dim)==3:
-							self.field.f.canon[space].body = bodyType.three
-						
-						
-					for (space,base) in enumerate(bases):
-						self.field.f.canon[space].basis = base
-						
-		return self	
-			
+							if len(dim)==1:
+								self.field.f.canon[space].body = bodyType.one
+							elif len(dim)==2:
+								self.field.f.canon[space].body = bodyType.two
+							elif len(dim)==3:
+								self.field.f.canon[space].body = bodyType.three
+						for (space,base) in enumerate(bases):
+							self.field.f.canon[space].basis = base
+		return self
+	
 	def block(self, blockDescs:[str]):
 		"""block memory allocations and some controls
 		
@@ -552,7 +597,6 @@ cdef class galaxy:
 		
 	def compress ( self, g : galaxy , vector : division = division.eigenVectors):
 		"""self-> g
-		
 		testing...
 		"""
 		cdef inta spatial[SPACE][SPACE]
