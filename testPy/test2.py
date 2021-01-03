89a94059b234356d939cf3fc5589277bfc3b31ba
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-01-03 07:27:49 -0600

    Create test2.py

---
+++ testPy/test2.py
@@ -0,0 +1,28 @@
+##create a galaxy then copy its definitions
+import andromedaPy as apy
+
+g = apy.galaxy()
+cs = [g.comps(40),g.comps(40),g.comps(40)]
+ds = [[g.dims(lattice = 0.333),g.dims(lattice = 0.333)],[g.dims(lattice = 0.333),g.dims(lattice = 0.333)],[g.dims(lattice = 0.333),g.dims(lattice = 0.333)]]
+ls = [1,1,1]
+bs = [g.bases('Sinc'),g.bases('Sinc'),g.bases('Sinc')]
+g.spaces(ls,cs,ds,bs)
+g.block(['total','train','copy','component','diagonal','total-parallel','matrixElement-parallel','multiply-parallel','permute','permute-parallel','transfer'])
+g.calculationInputs(RAMmax = 4,numVectors = 0,numNames = 0)
+g.fieldInputs(canonRank = 1,nStates = 1,OpIndex = 0)
+
+
+g.i()
+print(g.labels())
+print(g.comps())
+print(g.dims())
+print(g.bases())
+
+gg = apy.galaxy()
+gg.spaces(g.labels(),g.comps(),g.dims(),g.bases())
+gg.i()
+
+print(gg.labels())
+print(gg.comps())
+print(gg.dims())
+print(gg.bases())
