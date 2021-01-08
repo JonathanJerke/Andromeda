63bc4ee4a13dbc52064e06f2e616d997cd1154f9
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-01-07 19:44:02 -0600

    Update full2.py

--- testPy/full2.py
+++ testPy/full2.py
@@ -8,7 +8,6 @@ cs = [g.comps(25),g.comps(25)]
 ds = [[g.dims(lattice = 0.333)],[g.dims(lattice = 0.333),g.dims(lattice = 0.333)]]
 bs = [g.bases('Sinc'),g.bases('Sinc')]
 g.spaces(ls,cs,ds,bs)
-#g.block(['total','train','component','diagonal','total-parallel','matrixElement-parallel','multiply-parall>
 g.block(['component','diagonal','total-parallel','matrixElement-parallel','multiply-parallel','permute','permute-parallel','transfer'])
 
 g.calculationInputs(RAMmax = 1,numVectors = 0,numNames = 0)
@@ -21,4 +20,4 @@ g.full(f)
 print(f.isbooted())
 
 ##only canonRank = 1 works ATM
-f.compress(g,canonRank = 1)
+f.compress([[0,1,2]],g,canonRank = 1)
