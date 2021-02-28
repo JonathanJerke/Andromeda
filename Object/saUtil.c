278e3b42d259672e18e010f7e63ec3edb85bdd2b
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-02-28 14:01:50 -0600

    Add files via upload

--- Object/saUtil.c
+++ Object/saUtil.c
@@ -225,9 +225,9 @@ bodyType commandSA(  bodyType bd, inta act,   blockType cl,   blockType bl,inta 
                             op[3] = 3;
                             return one;
                         case tv4:
-                            op[0] = 2;
+                            op[0] = 3;
                             op[1] = 1;
-                            op[2] = 3;
+                            op[2] = 2;
                             op[3] = 0;
                             return one;
                         default:
