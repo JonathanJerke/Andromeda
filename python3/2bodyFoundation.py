0cb5e7c7491d209972d20e170ed268840871d006
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-04-06 18:24:27 -0500

    Create 2bodyFoundation.py

---
+++ python3/2bodyFoundation.py
@@ -0,0 +1,14 @@
+from numpy import *
+import pandas as pd
+import os
+from andromedaPy import galaxy
+if os.getenv('LAUNCH')==None :
+    print('warning,  bootup Jupyter after ignition.sh')
+from scipy import linalg
+import itertools
+
+m = [-1,0,1]
+n = [0.5]
+sg = pd.DataFrame(data = list(itertools.product(m,n,m,n,m,n,m,n,m,n,m,n)))
+sg['index'] = sg.index
+sg.to_csv('sg.csv',index = False)
