76497c8a373bec355f2ea81226c7044916433185
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-04-10 05:09:28 -0500

    Update ritzSC.sh

--- bash/ritzSC.sh
+++ bash/ritzSC.sh
@@ -4,7 +4,7 @@ if [ $# -eq 1 ]
     then
     dir=$1
      
-    file=$dir/e.riz
+    file=$dir/c.riz
     echo "*Body $file" > $file
     echo "*InputOutput" >> $file
     echo "read found/found" >> $file
@@ -18,7 +18,7 @@ if [ $# -eq 1 ]
     done
     for h in `cat spam`
         do
-            echo "sum $dir/E.$h.riz " >> $file
+            echo "sum $dir/C.$h.riz " >> $file
         done
     echo "control ritzPhase" >> $file
     echo ".InputOutput" >> $file
