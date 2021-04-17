bc3eb9b8fbfd602b38164a31494ac9e1956ac315
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-04-16 21:49:47 -0500

    Update tryThis.sh

--- bash/tryThis.sh
+++ bash/tryThis.sh
@@ -6,10 +6,10 @@ pathFinder.sh found 1 2 3 4 5
 cp commands  summa
 summa.sh 5 6X
 pathFinder.sh 6X 7 8 9 10 11 12 
-grep E commands | tail -n 36 > summa
+grep $krylovBuild commands | tail -n 36 > summa
 summa.sh 12 13X
 pathFinder.sh 13X `seq 14 24`
-grep E commands | tail -n 36 > summa
+grep $krylovBuild commands | tail -n 36 > summa
 summa.sh 24 25X
 pathFinder.sh 25X `seq 26 36`
 cp inc1 7/inc
