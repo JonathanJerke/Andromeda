5f6eb4cc63c593ecf1d2fd8b2abe92e0dee780fb
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-04-25 07:17:06 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-export PATH=$PATH:`pwd`/bash:`pwd`/Object:`pwd`/python3
+export LDFLAGS=-L`pwd`
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`
 export PYTHONPATH=$PYTHONPATH:`pwd`
+export PATH=$PATH:`pwd`/bash:`pwd`/Object:`pwd`/python3
