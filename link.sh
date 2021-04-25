a7ead1382713ea95e2857ca3f114f3237a4ed8ee
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-04-25 16:21:47 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+##compile first
+cd Object
 export LDFLAGS=-L`pwd`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`
+cd ..
+
+cd andromedaPy
 export PYTHONPATH=$PYTHONPATH:`pwd`
-export PATH=$PATH:`pwd`/bash:`pwd`/Object:`pwd`/python3
 python3 setup.py build_ext -i
+cd ..
