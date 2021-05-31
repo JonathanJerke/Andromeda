4f7c50464e828957ceaca7da6b6ba319656cbc67
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-05-31 16:06:20 -0500

    Update prevKrylovD.sh

--- bash/prevKrylovD.sh
+++ bash/prevKrylovD.sh
@@ -18,5 +18,23 @@ if [ $# -eq 3 ]
         echo ".Body" >> $file
         go.sh $file
     done
+    
+    if [ -e include ] 
+    then
+    for i in `cat include`
+        do
+        file=$dir/$krylovBuild.kry.$i
+        echo "*Body $file" > $file
+        echo "*InputOutput" >> $file
+        echo "read found/found" >> $file
+        echo "read $dir/stage " >> $file
+        echo "vector $prev-$i" >> $file
+        echo "control buildPhase ">> $file
+        echo ".InputOutput" >> $file
+        echo ".Body" >> $file
+        go.sh $file
+    done
+    fi
+    
 fi
 
