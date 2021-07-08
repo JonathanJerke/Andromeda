1820f7e644393e5a424dfb4b1f2e3fc7b0ce8d1e
Jonathan Jerke <jonathanjerke@aya.yale.edu>
2021-07-08 18:52:42 -0500

    Update start.sh

--- bash/start.sh
+++ bash/start.sh
@@ -21,8 +21,7 @@ if [ $# -eq 2 ]
     echo 1 > hosts
     echo 1 > ppn
     echo 10000 > spn
-    echo "ham" > sums
-#MODLULARIZE good luck with that!    python3 $LAUNCH/../python3/touch.py D
+  #MODLULARIZE good luck with that!    python3 $LAUNCH/../python3/touch.py D
 #    python3 $LAUNCH/../python3/touch.py found/T
 ##
 if [ -f ../cnfg ]
@@ -34,11 +33,17 @@ fi
 
 if [ -f ../cnfg ]
     then
-    line=`grep spam ../cnfg`
+    line=`grep ham ../cnfg`
     spam=`header2.sh $line`
     seq 1 $spam  > spam
 fi
 
+if [ -f ../ops ]
+    then
+    echo "spam ../ops" > sums
+else
+    echo "spam" > sums
+fi
 
 else
     echo "start.sh <name> SA"
